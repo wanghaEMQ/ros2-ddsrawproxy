541cc55253477936df44bc31abb1a184b8b166de
wangha <wanghamax@gmail.com>
2023-10-12 13:51:28 +0800

    * NEW [src] Add publisher and writer.
    Signed-off-by: wangha <wanghaemq@gmail.com>

--- src/subscriber_member_function.cpp
+++ src/subscriber_member_function.cpp
@@ -1,5 +1,7 @@
+#include <dds/ddsc/dds_public_impl.h>
 #include <functional>
 #include <memory>
+#include <stdio.h>
 
 #include "rclcpp/rclcpp.hpp"
 #include "tutorial_interfaces/msg/num.hpp"                                       // CHANGE
@@ -23,21 +25,79 @@ private:
   void topic_callback(const tutorial_interfaces::msg::Ddstype & msg) const  // CHANGE
   {
     RCLCPP_INFO_STREAM(this->get_logger(), "Get int32_test: '" << msg.int32_test << "'");     // CHANGE
+    // dds_write();
   }
   rclcpp::Subscription<tutorial_interfaces::msg::Ddstype>::SharedPtr subscription_;  // CHANGE
 };
 
+#define ROS2DDS_FROM "topic1"
+#define ROS2DDS_TO "topic1"
+
+#define DDS2ROS_FROM "topic2"
+#define DDS2ROS_TO "topic2"
+
+#define DDS_PARTITION "testpart"
+
+static const char *partition[1];
+partition[0] = {DDS_PARTITION};
+
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
-  rclcpp::shutdown();
 
   dds_entity_t participant;
   participant = dds_create_participant(0, NULL, NULL);
   if (participant < 0)
-    printf("Error");
-  printf("YES!");
+    fprintf(stderr, "Error");
+  fprintf(stderr, "YES!");
+
+  dds_entity_t publisher;
+  dds_qos_t *  qospub;
+
+  /* Qos for Publisher */
+  qospub = dds_create_qos();
+  dds_qset_partition(qospub, 1, partition);
+
+  /* Create the Publisher. */
+  publisher = dds_create_publisher(participant, qospub, NULL);
+  if (publisher < 0)
+    fprintf(stderr, "Error");
+  dds_delete_qos(qospub);
+  fprintf(stderr, "YES!");
+
+  uint32_t rc = 0;
+  dds_entity_t topicw;
+  dds_qos_t *qosw;
+  dds_entity_t writer;
+
+  /* Topic for writer */
+  topicw = dds_create_topic(
+    participant, ddsdesc, ROS2DDS_TO, NULL, NULL);
+  if (topicw < 0) {
+    DDS_FATAL("dds_create_topic: %s\n", dds_strretcode(-topicw));
+    return topicw;
+  }
+
+  /* Qos for Writer */
+  qosw = dds_create_qos();
+  dds_qset_reliability(qosw, DDS_RELIABILITY_RELIABLE, DDS_SECS(10));
+
+  /* Create a Writer */
+  writer = dds_create_writer(publisher, topicw, qosw, NULL);
+  if (writer < 0) {
+    DDS_FATAL("dds_create_writer: %s\n", dds_strretcode(-writer));
+    return writer;
+  }
+  dds_delete_qos(qosw);
+
+  rc = dds_set_status_mask(writer, DDS_PUBLICATION_MATCHED_STATUS);
+  if (rc != DDS_RETCODE_OK) {
+    DDS_FATAL("dds_set_status_mask: %s\n", dds_strretcode(-rc));
+    return rc;
+  }
+
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
 
   return 0;
 }
